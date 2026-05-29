7298be01e40e32816624a09252e8cbb74deb04c8
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-05-29 14:42:33 -0400

    feat: implement responsive canvas scaling to support various screen sizes

--- js/java.js
+++ js/java.js
@@ -1,17 +1,18 @@
 import Juego from './juego.js';
 const canvas = document.getElementById('pantallaJuego');
 let ctxt = canvas.getContext('2d');
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
-const gameWidth = 0.8*windowWidth;
-const gameHeight = windowHeight;
-//ctxt.fillStyle = '#538dfb'
-//ctxt.fillRect(300, 200, 200, 200);
+
+// Ajustar el tamaño del canvas al tamaño del contenedor responsivo
+const container = document.getElementById('game-container');
+const gameWidth = container.clientWidth;
+const gameHeight = container.clientHeight;
+
+canvas.width = gameWidth;
+canvas.height = gameHeight;
+
 const juego = new Juego(gameWidth, gameHeight);
-//juego.inicio();
 let tiempo = 0;
-//img
-//const balon = document.getElementById('balon');
+
 function bucleGame(tiempoStamp) {
     let deltaT = tiempoStamp - tiempo;
     tiempo = tiempoStamp;
